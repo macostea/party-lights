6171993dd7fecb034a50a3ab7b47964b21e15b2a
Mihai Costea <mihai.andrei.costea@icloud.com>
2017-08-23 22:18:25 +0300

    Remove slip reading from BTLE write callback.\n Just forward the message

--- rpi/btle/next_char.go
+++ rpi/btle/next_char.go
@@ -2,25 +2,13 @@ package btle
 
 import (
 	"github.com/currantlabs/ble"
-	"github.com/Lobaro/slip"
-	"bytes"
-	"io"
-	"log"
 )
 
 func NewNextPatternCharacteristic(nextPatternChannel chan<- []byte) *ble.Characteristic {
 	c := ble.NewCharacteristic(ble.MustParse("720EEE67-CCE2-430D-BB40-4815F3B69295"))
 
 	c.HandleWrite(ble.WriteHandlerFunc(func(req ble.Request, rsp ble.ResponseWriter) {
-		reader := slip.NewReader(bytes.NewReader(req.Data()))
-
-		packet, _, err := reader.ReadPacket()
-
-		if err != io.EOF {
-			log.Printf("Failed to receive packet, %s", err)
-			return
-		}
-
+		packet := req.Data()
 		nextPatternChannel <- packet
 	}))
 
